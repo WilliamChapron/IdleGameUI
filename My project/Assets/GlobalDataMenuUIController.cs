8585ad05abc0b739d1c5f49e6d93fef29213df62
William's Student <102622446+WilliamChapron@users.noreply.github.com>
2024-12-13 13:20:16 +0100

    [ADD] GlobalDataUIMenu

---
+++ My project/Assets/GlobalDataMenuUIController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GlobalDataMenuUIController : MonoBehaviour
+{
+    [SerializeField] private UIDocument uIDocument; // Référence au UIDocument
+
+    private VisualElement _generalDataContainer;
+    private Button _toggleButton;
+
+    private VisualElement _contentContainer;
+
+    private bool _isOpen = true;
+
+    void OnEnable()
+    {
+
+        var root = uIDocument.rootVisualElement;
+
+        _generalDataContainer = root.Q<VisualElement>("generalDataContainer");
+        _toggleButton = root.Q<Button>("toggleButton");
+
+        _toggleButton.clicked += ToggleMenu;
+    }
+
+    private void ToggleMenu()
+    {
+        _isOpen = !_isOpen;
+
+        if (_isOpen)
+        {
+            _generalDataContainer.RemoveFromClassList("closed");
+            _toggleButton.RemoveFromClassList("closed");
+        }
+        else
+        {
+            _generalDataContainer.AddToClassList("closed");
+            _toggleButton.AddToClassList("closed");
+        }
+    }
+}
